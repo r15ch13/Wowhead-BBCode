a62c3e8c6cbce5da3e4f25685ed8db18d1ac7a70
Richard Kuhnt <r15ch13+git@gmail.com>
2014-07-02 03:48:56 +0200

    Update for WCF 2

--- README.md
+++ README.md
@@ -25,18 +25,5 @@ Example:
 It seams there is a bug in Wowheads power.js. If you add an item with gems/enchantments or whatsoever the script breaks and only items before the broken item will be displayed correctly. Avoid using gems/enchantments etc. until Wowhead fixes this problem.
 
 License
-----------
-Copyright 2013 Richard 'r15ch13' Kuhnt
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/lgpl-3.0>.
+-------
+[The MIT License (MIT)](http://r15ch13.mit-license.org/)
---
+++ src/tar/files/lib/system/bbcode/WowheadBBCode.class.php
@@ -0,0 +1,51 @@
+<?php
+namespace wcf\system\bbcode;
+use wcf\system\WCF;
+use wcf\system\bbcode\AbstractBBCode;
+use wcf\util\StringUtil;
+
+/**
+ * @package de.r15ch13.wcf.bbcode.wowhead
+ * @author Richard Kuhnt
+ * @license MIT License (http://r15ch13.mit-license.org/)
+ * @category WCF
+ * @see http://www.wowhead.com/tooltips
+ */
+class WowheadBBCode extends AbstractBBCode
+{
+    /**
+     * @see wcf\system\bbcode\IBBCode::getParsedTag()
+     */
+    public function getParsedTag(array $openingTag, $content, array $closingTag, BBCodeParser $parser)
+    {
+        $templateVariables = array(
+            'url' => $content,
+            'related' => '',
+            'language' => 'www',
+            'type' => '',
+            'id' => 0,
+        );
+
+        if(preg_match('/^https?:\/\/(.+?)?\.?(?:wowhead|thottbot|hearthhead)\.com(?:\:\d+)?\/\??(item|quest|spell|achievement|transmog-set|statistic|npc|object|petability|hearthstone\/card|card|hsachievement|cardback|deck|mechanic)=([0-9]+)$/', $content, $matches))
+        {
+            if(count($matches) >= 4)
+            {
+                $templateVariables['url'] = $matches[0];
+                $templateVariables['language'] = $matches[1];
+                $templateVariables['type'] = $matches[2];
+                $templateVariables['id'] = $matches[3];
+            }
+        }
+
+        // Related advanced usage
+        // @see http://www.wowhead.com/tooltips#related-advanced-usage
+        if(isset($openingTag['attributes'][0]))
+        {
+            $templateVariables['related'] = trim($openingTag['attributes'][0]);
+        }
+
+        WCF::getTPL()->assign($templateVariables);
+
+        return WCF::getTPL()->fetch('wowheadBBCode');
+    }
+}
